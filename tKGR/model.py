6e306e07c8969e8199cb9af47f1f652274c5144c
Peng <peng.chen@tum.de>
2020-06-16 17:17:30 +0200

    + transition matrix for node representation update in previous seleted edges + use -2, -1 to select node_idx for source nodes and target nodes

--- tKGR/model.py
+++ tKGR/model.py
@@ -307,9 +307,9 @@ class AttentionFlow(nn.Module):
         attending_node_attention = torch.squeeze(torch.sparse.mm(trans_matrix_sparse, node_attention.unsqueeze(1)))
 
         print("edges for message passing:")
-        print(pruned_edges[:, [6, 7]])
-        sparse_index_rep = torch.from_numpy(pruned_edges[:, [6, 7]]).to(torch.int64).to(self.device)
-        sparse_index_identical = torch.from_numpy(np.setdiff1d(np.arange(num_nodes), pruned_edges[:, 6])).unsqueeze(1).repeat(1,2).to(self.device)
+        print(pruned_edges[:, [-2, -1]])
+        sparse_index_rep = torch.from_numpy(pruned_edges[:, [-2, -1]]).to(torch.int64).to(self.device)
+        sparse_index_identical = torch.from_numpy(np.setdiff1d(np.arange(num_nodes), pruned_edges[:, -2])).unsqueeze(1).repeat(1,2).to(self.device)
         sparse_index_rep = torch.cat([sparse_index_rep, sparse_index_identical], axis=0)
         sparse_value = torch.cat([transition_logits_pruned_softmax, torch.ones(len(sparse_index_identical)).to(self.device)])
         trans_matrix_sparse_rep = torch.sparse.FloatTensor(sparse_index_rep.t(), sparse_value, torch.Size([num_nodes, num_nodes])).to(self.device)
@@ -340,10 +340,10 @@ class AttentionFlow(nn.Module):
 
             print("edges for messaage passing:")
             print(selected_edges[:, [6, 7]])
-            sparse_index_rep = torch.from_numpy(selected_edges[:, [6, 7]]).to(torch.int64).to(self.device)
-            sparse_index_rep = torch.cat(
-                [sparse_index_rep, torch.arange(num_nodes).unsqueeze(1).repeat(1, 2).to(self.device)], axis=0)
-            sparse_value = torch.cat([transition_logits - 1, torch.ones(num_nodes).to(self.device)])
+            sparse_index_rep = torch.from_numpy(selected_edges[:, [-2, -1]]).to(torch.int64).to(self.device)
+            sparse_index_identical = torch.from_numpy(np.setdiff1d(np.arange(num_nodes), selected_edges[:, -2])).unsqueeze(1).repeat(1, 2).to(self.device)
+            sparse_index_rep = torch.cat([sparse_index_rep, sparse_index_identical], axis=0)
+            sparse_value = torch.cat([transition_logits, torch.ones(len(sparse_index_identical)).to(self.device)])
             trans_matrix_sparse_rep = torch.sparse.FloatTensor(sparse_index_rep.t(), sparse_value,
                                                                torch.Size([num_nodes, num_nodes])).to(self.device)
             updated_memorized_embedding = torch.sparse.mm(trans_matrix_sparse_rep, memorized_embedding)
