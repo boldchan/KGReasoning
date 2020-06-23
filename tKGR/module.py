512b382319407cdb5a014db3a825c67b6d897aea
Zhen Han <hanzhen02111@163.com>
2020-06-23 11:07:29 +0200

    fix_entity_specific_temporal_emb_bug

--- tKGR/module.py
+++ tKGR/module.py
@@ -199,20 +199,20 @@ class TimeEncode(torch.nn.Module):
         refer to Self-attention with Functional Time Representation Learning for more detail
         '''
         super(TimeEncode, self).__init__()
-        time_dim = expand_dim
+        self.time_dim = expand_dim
         self.entity_specific = entity_specific
 
         if entity_specific:
-            self.basis_freq = torch.nn.Parameter(torch.from_numpy(1 / 10 ** np.linspace(0, 9, time_dim)).float().unsqueeze(dim=0).repeat(num_entities, 1))
-            self.phase = torch.nn.Parameter(torch.zeros(time_dim).float().unsqueeze(dim=0).repeat(num_entities, 1))
+            self.basis_freq = torch.nn.Parameter(torch.from_numpy(1 / 10 ** np.linspace(0, 9, self.time_dim)).float().unsqueeze(dim=0).repeat(num_entities, 1))
+            self.phase = torch.nn.Parameter(torch.zeros(self.time_dim).float().unsqueeze(dim=0).repeat(num_entities, 1))
         else:
-            self.basis_freq = torch.nn.Parameter(torch.from_numpy(1 / 10 ** np.linspace(0, 9, time_dim)).float())
-            self.phase = torch.nn.Parameter(torch.zeros(time_dim).float())
+            self.basis_freq = torch.nn.Parameter(torch.from_numpy(1 / 10 ** np.linspace(0, 9, self.time_dim)).float()) # shape: num_entities * time_dim
+            self.phase = torch.nn.Parameter(torch.zeros(self.time_dim).float())
 
     def forward(self, ts, entities = None):
         '''
         :param ts: [batch_size, seq_len]
-               entities: which entities do we extract their temporal embeddings?
+               entities: which entities do we extract their temporal embeddings.
         :return: [batch_size, seq_len, time_dim]
         '''
         batch_size = ts.size(0)
@@ -220,10 +220,10 @@ class TimeEncode(torch.nn.Module):
         ts = torch.unsqueeze(ts, dim=2)
         # print("Forward in TimeEncode: ts is on ", ts.get_device())
         if self.entity_specific:
-            map_ts = ts * self.basis_freq[entities]  # [batch_size, seq_len, time_dim]
+            map_ts = ts * self.basis_freq[entities].unsqueeze(dim=1)  # self.basis_freq[entities]:  [batch_size, time_dim]
             map_ts += self.phase[entities]
         else:
-            map_ts = ts * self.basis_freq.view(1, 1, -1)  # [batch_size, seq_len, time_dim]
+            map_ts = ts * self.basis_freq.view(1, 1, -1)  # [batch_size, 1, time_dim]
             map_ts += self.phase.view(1, 1, -1)
         harmonic = torch.cos(map_ts)
         return harmonic
